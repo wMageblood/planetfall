fc79fd030b6271ea7885aa160bbc2760ef5e254c
wMageblood <enzo.luciano@outlook.es>
2026-01-27 21:05:40 -0600

    fixed properties

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # __POKEVIEWER__
 
-__POKEVIEWER__ fue creado con la intencion de aprender a utilizar APIs. Consta de ejemplos simples en los cuales podemos ver como importar un Pokemon en base a su nombre o a su numero.
+__POKEVIEWER__ fue creado con la intención de aprender a utilizar APIs. Consta de ejemplos simples en los cuales podemos ver como importar un Pokemón en base a su nombre o a su número.
 
 ### To-Do 👓 :
 1. ~~Previous y Next button~~. (kinda)
 2. Cambiar los colores de la pagina.
-3. Descripcion por cada Pokemon.
-4. ~~Poner tildes al __README__.~~ ⚠️
+3. ~~Descripción/Categoría/Género por cada Pokemon.~~
+4. ~~Poner tildes al __README__.~~
 5. ~~Crear rutas para cada componente.~~
-6. ~~Name/Number search box ➡️ orange magnifier~~
+6. ~~Name/Number search box ➡️ orange magnifier.~~
+7. Land Page.
+8. Renombrar componentes a algo que tenga sentido.
+9. Asignar rutas que tengan sentido.
+10. Recrear el componente llamado "elemento1".
 
-Este proyecto tiene fecha de terminacion para el __27/01/2026__, posteriormente no sera mas actualizado.
+Este proyecto tiene fecha de terminación para el __xx/xx/2026__, posteriormente no sera más actualizado.
 
 ## POKEVIEWER
 
-Parece que __POKEVIEWER__ esta cambiando un poco su target, estoy transformandolo en una especia de POKEDEX en donde cada Pokemon tiene su propio path.
+Parece que __POKEVIEWER__ está cambiando un poco su target, estoy transformándolo en una especia de POKEDEX en donde cada Pokemón tiene su propio path y propio componente.
--- src/components/WikiPokemonCard/WikiPokemonCard.tsx
+++ src/components/WikiPokemonCard/WikiPokemonCard.tsx
@@ -22,10 +22,13 @@ const WikiPokemonCard = (data: PokemonData) => {
   const weightConverter = (weight: number) => Math.ceil(((weight * 2.2046) / 10));  // kg to lb
   const upperCaseFirstLetter = (letter: string) => letter.slice(0, 1).toUpperCase() + letter.slice(1); //uppercase first letter of the ability
 
-  console.log(data.name)
+  const valueFetcher = (key: string) => {
+    const pokemon = firstGenPokemonList.find(({ name }) => name.toLowerCase() === data.name?.toLowerCase());
+    return pokemon ? pokemon[key as keyof typeof pokemon] : null;
+  }
 
   return (
-    <div className={`mx-4`}>
+    <div className={`mx-4 mt-2`}>
       <div className={`flex w-full rounded-sm bg-tailwindBlueNavigation p-4`}>
         <div className={`w-3xl`}>
           <img alt={`${data.name}`} src={`${data.sprite || null}`} />
@@ -33,7 +36,7 @@ const WikiPokemonCard = (data: PokemonData) => {
         <div className={`font-semibold text-xl`}>
           <p className={`ml-2 uppercase font-bold text-2xl tracking-tighter`}>{`${data.name} #${data.id}`}</p>
           <p className={`mt-2 ml-2 mb-2 text-gray-200`}>
-            {firstGenPokemonList.find(({ name }) => name.toLowerCase() == data.name)?.description || "description not found"}
+            {valueFetcher('description')}
           </p>
           <div className={`grid grid-cols-2 w-lg ml-2 my-10`}>
             <p className={`font-normal text-gray-300`}>Height:</p>
@@ -43,10 +46,10 @@ const WikiPokemonCard = (data: PokemonData) => {
             <p className={`font-normal text-gray-300`}> Ability:</p>
             <p className={`font-normal text-gray-300`}> Gender:</p>
             <p className={`mb-6`}>{`${upperCaseFirstLetter(data.abilities)}`}</p>
-            <p className={``}>Unknown</p>
+            <p className={``}>{valueFetcher('gender')}</p>
             <p className={`font-normal text-gray-300`}>Category:</p>
             <span></span>
-            <p className={``}>Generic</p>
+            <p className={``}>{valueFetcher('category')}</p>
           </div>
           <div className={`ml-2 mb-6`}>
             <span className={`font-normal text-gray-300`}>Type: </span><button className={`${backGroundColor(data.type)} ml-2 px-3 py-1 rounded-sm ring-2`}>{upperCaseFirstLetter(data.type)}</button>
@@ -55,7 +58,6 @@ const WikiPokemonCard = (data: PokemonData) => {
             <span className={`font-normal text-gray-300 mr-2`}>Weaknesses: </span> <div> {pokemonWeaknessesTwo(data.type).map((x, key) =>
               <button key={key} className={`${backGroundColor(x)} ml-2 px-3 py-1 rounded-sm ring-2`}>{x.toUpperCase()}</button>)}
             </div>
-
           </div>
         </div>
       </div>
@@ -64,12 +66,3 @@ const WikiPokemonCard = (data: PokemonData) => {
 }
 
 export default WikiPokemonCard
-
-
-/*
-        <div className="relative flex py-5 items-center">
-          <div className="grow border-t border-gray-400"></div>
-            <span className="shrink mx-4 text-gray-400">Content</span>
-          <div className="grow border-t border-gray-400"></div>
-        </div>
-*/
