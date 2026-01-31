b71890c52c67ea198c23fcb16cca6fe897fdee9f
wMageblood <enzo.luciano@outlook.es>
2026-01-30 20:58:41 -0600

    tidy?

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,14 +1,14 @@
 function NavigationBar() {
   return (
-    <nav className={`mt-25 `}>
+    <nav className={`mt-2`}>
       <div className={`flex justify-center`}>
         <div className={`bg-linear-to-b from-mainBorder1 to-mainBorder2 w-auto rounded-full h-12 flex items-center p-[3px] inset-shadow-nav`}>
           <div className={`text-gray bg-linear-to-t from-navigationBody1 to-navigationBody2 rounded-full w-full h-full flex items-center p-[3px] inset-shadow-md`}>
             {/* <div className={`text-gray-300 bg-linear-to-t from-navigationBody1 to-navigationBody2 flex items-center w-full h-full p-[3px] rounded-full`}> */}
-            <a href="#" className={`hover-navigation-button`}>Projects</a>
-            <a href="#" className={`hover-navigation-button ml-2`}>Gallery</a>
-            <a href="#" className={`hover-navigation-button mx-2`}>Studio</a>
-            <a href="#" className={`hover-navigation-button`}>Profile</a>
+            <a href="#" className={`hover-navigation-button`}>Pokeviewer</a>
+            <a href="/elemento2" className={`hover-navigation-button ml-2`}>Pokesearch</a>
+            <a href="#" className={`hover-navigation-button mx-2`}>Pokestudio</a>
+            <a href="#" className={`hover-navigation-button`}>Pokeprofile</a>
             {/* </div> */}
           </div>
         </div>
@@ -18,18 +18,3 @@ function NavigationBar() {
 }
 
 export default NavigationBar;
-
-
-
-// <nav className="place-items-center mt-2">
-//   <div className="">
-//     <div className="bg-linear-to-tr from-navigationBlackGray to-navigationGray flex items-center rounded-4xl w-auto h-15 text-gray-300 font-bold p-[3px]">
-//       <div className={`rounded-full bg-linear-to-bl from-navigationBlackGray to-navigationGray inset-shadow-md h-full w-full flex items-center justify-center`}>
-//         <a href="/" className=" ml-6 mx-6 tracking-tighter">POKESTADIUM</a>
-//         <a href="/elemento1" className="mx-4 tracking-tighter">POKEHUNTER</a>
-//         <a href="/elemento2" className="mx-4 tracking-tighter">POKEVIEWER</a>
-//         <a href="" className="mr-6 mx-4 tracking-tighter">POKECLINIC</a>
-//       </div>
-//     </div>
-//   </div>
-// </nav>
