15c9c59fed65926bed49d401dfe2d58a96fe18b0
wMageblood <enzo.luciano@outlook.es>
2026-02-02 19:59:55 -0600

    tidyyyy

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,7 +5,7 @@ function NavigationBar() {
         <div className={`bg-linear-to-b from-mainBorder1 to-mainBorder2 w-auto rounded-full h-12 flex items-center p-[3px] inset-shadow-nav`}>
           <div className={`text-gray bg-linear-to-t from-navigationBody1 to-navigationBody2 rounded-full w-full h-full flex items-center p-[3px] inset-shadow-md`}>
             {/* <div className={`text-gray-300 bg-linear-to-t from-navigationBody1 to-navigationBody2 flex items-center w-full h-full p-[3px] rounded-full`}> */}
-            <a href="#" className={`hover-navigation-button`}>Pokeviewer</a>
+            <a href="#" className={`hover-navigation-button`}>POKEMONVIEWER</a>
             <a href="/elemento2" className={`hover-navigation-button ml-2`}>Pokesearch</a>
             <a href="#" className={`hover-navigation-button mx-2`}>Pokestudio</a>
             <a href="#" className={`hover-navigation-button`}>Pokeprofile</a>
