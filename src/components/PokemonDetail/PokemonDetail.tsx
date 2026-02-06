a639cda56796948ef4cea2f9de9518ccbfba87ce
wMageblood <enzo.luciano@outlook.es>
2026-02-06 16:00:34 -0600

    fixed buttons

--- src/components/PokemonDetail/PokemonDetail.tsx
+++ src/components/PokemonDetail/PokemonDetail.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import WikiPokemonCard from "../WikiPokemonCard/WikiPokemonCard";
+import firstGenPokemonList from "../../constants/firstGenPokemonsList";
 
 
 const pokemonData = {
@@ -39,14 +40,48 @@ const PokemonDetail = () => {
     }
   }, [pokemonName]);
 
+  // const handleClick = (id: number) => {
+
+  //   console.log(`${properties.id} <- properties.id`)
+
+  //   const nextPokemon = properties.id + 1;
+
+  //   console.log(nextPokemon + " <- nextPokemon")
+
+
+  //   const valueFetcher = (key: string) => {
+  //     const findNextPokemon = firstGenPokemonList.find(({ number }) => number + 1 === properties.id)
+  //     return findNextPokemon ? findNextPokemon[key as keyof typeof findNextPokemon] : null;
+  //   }
+
+  //   console.log(valueFetcher('name'))
+
+  //   return valueFetcher('name')
+
+  // }
+
+  const fetchPreviousPokemon = (key: string) => {
+    const findNextPokemon = firstGenPokemonList.find(({ number }) => number === properties.id - 1)
+    return findNextPokemon ? findNextPokemon[key as keyof typeof findNextPokemon] : null;
+  }
+
+  const fetchNextPokemon = (key: string) => {
+    const findNextPokemon = firstGenPokemonList.find(({ number }) => number === properties.id + 1)
+    return findNextPokemon ? findNextPokemon[key as keyof typeof findNextPokemon] : null;
+  }
+
+  // const pokemon = firstGenPokemonList.find(({ name }) => name.toLowerCase() === data.name?.toLowerCase());
+
+
 
   //fetch current pokemon, then handleClick function using ID + 1 or ID - 1
 
   return (
     <div className={`mt-4`}>
       <div className={`flex space-x-2 mx-4 *:h-10 *:rounded-sm *:w-1/2 *:font-Geist *:tracking-tighter`}>
-        <button className={`bg-blue-500`}>« PREVIOUS</button>
-        <button className={`bg-green-500`}>NEXT »</button>
+        <a href={`/${fetchPreviousPokemon('name')}`} className={`bg-blue-500 flex items-center justify-center`}>PREVIOUS {fetchPreviousPokemon('name')}</a>
+        <a href={`/${fetchNextPokemon('name')}`} className={`bg-green-500 flex items-center justify-center`}>NEXT {fetchNextPokemon('name')}</a>
+
       </div>
       <WikiPokemonCard
         id={properties.id}
